568ead181541b9f87e666f7b07c6708e173cdea4
LeonardoAxEM <leonardoaem22@gmail.com>
2025-08-17 10:35:52 -0600

    Added new theme 1.2 Matematic Models

--- Menu/urls.py
+++ Menu/urls.py
@@ -5,4 +5,5 @@ from Menu import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('Sistemas-de-Comunicacion/', views.page_sc, name='page_sc'),
+    path('Modelos-Matematicos/', views.mm, name='mm'),
 ]
--- Menu/views.py
+++ Menu/views.py
@@ -6,3 +6,7 @@ def home(request):
 def page_sc(request):
     practica = Practicas.objects.get(id=1)
     return render(request, 'Pages/sc.html', {'practica': practica})
+
+def mm(request):
+    practica = Practicas.objects.get(id=2)
+    return render(request, 'Pages/tema1_2.html', {'practica': practica} )
